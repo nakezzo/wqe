9d3255afb4d6545e121069359862df1f81034ffc
nakezzo <ivanshemuda2@gmail.com>
2025-03-15 22:15:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = '7844044229:AAEr3MTn-lLeuPjyPx_tvyEAltxXA8rb9DE'  # Токен бота
 
-admins = [-1002515048022]  # ID админов
+admins = [-1002631392356]  # ID админов
 
 support = '@cavmpt'  # Username поддержки
